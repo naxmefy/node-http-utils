c4d0699d70112bfc9d231f8fe389a2950dffd61e
Matt <m.neundorf@live.de>
2016-08-27 14:22:38 +0000

    fix exports

--- src/koa/index.js
+++ src/koa/index.js
@@ -1,1 +1,2 @@
 export * as controllers from './controllers'
+export * as middlewares from './middlewares'
--- test/lib.spec.js
+++ test/lib.spec.js
@@ -1,17 +1,81 @@
 import * as lib from '../src'
 
 describe('Utils', function () {
-  const tests = [
+  const libexp = [
     'def',
     'env',
     'types',
     'mongoose',
+    'koa',
     'extend'
   ]
   
-  tests.forEach( test => {
-    it(`should have property ${test}`, function () {
-      lib.should.have.property(test)
+  libexp.forEach(e => {
+    it(`should have property ${e}`, function () {
+      lib.should.have.property(e)
+    })
+  })
+  
+  describe('extend', function () {
+    const extendexp = [
+      'KoaRouter'
+    ]
+    
+    extendexp.forEach(e => {
+      it(`should have property ${e}`, function () {
+        lib.extend.should.have.property(e)
+      })
+    })
+  })
+  
+  describe('mongoose', function () {
+    const mongooseexp = [
+      'cleanObject',
+      'schemaFromObject'
+    ]
+    
+    mongooseexp.forEach(e => {
+      it(`should have property ${e}`, function () {
+        lib.mongoose.should.have.property(e)
+      })
+    })
+  })
+  
+  describe('koa', function () {
+    const koaexp = [
+      'controllers',
+      'middlewares'
+    ]
+    
+    koaexp.forEach(e => {
+      it(`should have property ${e}`, function () {
+        lib.koa.should.have.property(e)
+      })
+    })
+    
+    describe('controllers', function () {
+      const koacontrollersexp = [
+        'AppController',
+        'ResourceController'
+      ]
+      
+      koacontrollersexp.forEach(e => {
+        it(`should have property ${e}`, function () {
+          lib.koa.controllers.should.have.property(e)
+        })
+      })
+    })
+    
+    describe('middlewares', function () {
+      const koamiddlewaresexp = [
+        'error'
+      ]
+      
+      koamiddlewaresexp.forEach(e => {
+        it(`should have property ${e}`, function () {
+          lib.koa.middlewares.should.have.property(e)
+        })
+      })
     })
   })
 })
